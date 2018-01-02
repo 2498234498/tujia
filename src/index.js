ca8181d09b35c8a40506099aa1ba0d40ce1ad3d7
2498234498 <CSDN passport2498234498@qq.com>
2018-01-02 17:20:20 +0800

    项目框架

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React,{Component} from "react"
+import ReactDOM from "react-dom"
+
+class Header extends Component {
+    render(){
+        return (
+            <div>hello world</div>
+        )
+    }
+}
+
+ReactDOM.render(<Header />,document.getElementById("root"))
